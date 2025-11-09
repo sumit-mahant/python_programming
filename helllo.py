38aa5e1cf2ab0f7006256ad61f76b071c87f69e4
sumit mahant <sumitmahant31@gmail.com>
2025-11-10 01:37:01 +0530

    jrkjf

---
+++ helllo.py
@@ -0,0 +1,16 @@
+print("sumit is good boy akashs is also a good boy ")
+print('sumit is good boy \n"akashs" is also a good boy' )
+print('sumit is good boy\t"akashs" is also a good boy' )
+str1="sumit is the best coder in the world"
+str2="mahant is the cast of sumit which realy dose not matter "
+print(str1+str2)
+print(len(str1))
+print(len(str1+str2))
+print(str1[2:5])
+print(str1[-5:-2])  #wor
+print(str1[-7:])
+print(str1.endswith("world"))
+print(str1.capitalize())
+print(str1.center(100))
+print(str1.count(""))
+print(str1.casefold())
---
+++ loops.py
@@ -0,0 +1,13 @@
+
+
+a=float(input("Enter your age:"))
+print("your age is ",a)
+if(a<18):
+    print("you can not drive")
+elif(a>70):
+    print("do you want to dei oldman ?") 
+    if(a>120):
+        print("")   
+else:
+    print('you can drive')
+print("be careful.")       
