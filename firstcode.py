28c7c8e8f199b7ca51333d6fbc5c4f626a817682
sumit mahant <sumitmahant31@gmail.com>
2025-11-19 15:16:05 +0530

    notes

---
+++ firstcode.py
@@ -0,0 +1,29 @@
+#Lecture 1 of apna college 
+print("hello world ")
+print(23)
+print(23+45)
+print("sumit is my name,My age is 19.")
+# \n is for line change
+print("sumit is my name\nMy age is 19.")
+#VARIABLES 
+# "=" is a assignment Operator
+name="sumit" #DATATYPE="sting"
+age=19 #datatype=int
+age2=age
+print(age2)
+price=30.23 #DATATYPE=float
+print("my name is :",name)
+print("my age is:",age)
+print(price)
+# DATATYPE
+str3="paro."#sting
+number=20#int
+point=20.345#float
+old=False#bool (T, F should be in capital!)
+r=None#Nonetype
+print(type(str3))
+print(type(number))
+print(type(point))
+print(type(old))
+print(type(r))
+
