c1b00a3eec8285931a44f11dc5fcf02cbb0df27b
sumit mahant <sumitmahant31@gmail.com>
2025-12-30 15:45:53 +0530

    cool
    so you

--- if-els.py
+++ if-els.py
@@ -5,13 +5,41 @@
 # else:
 #     print("your age is",age,"you can not drive!")
 
-num=int(input("enter the value of num:" ))
+# num=int(input("enter the value of num:" ))
 
+# if(num<0):
+#     print("the number is negative.")
+# elif(num==0):
+#     print("number is 0.")
+# elif(num==999):
+#     print("number is special.")
+# else:
+#     print("the number is positive.")  
+# print("I am so happy .")      
+  
+
+num=0
 if(num<0):
     print("the number is negative.")
-elif(num==0):
-    print("number is 0.")
-
+elif(num>0):
+    if(num<=10):
+        print("number is between 1-10.")
+    elif(num>10 and num<=20):
+        print("the number is between 10 and 20.")
+    else:
+        print("number is greater than 20.")
 else:
-    print("the number is positive.")    
-  
+    print("number is 0 ")            
+
+#     a=float(input("Enter your age:"))
+# print("your age is ",a)
+# if(a<18):
+#     print("you can not drive")
+# elif(a>70):
+#     print("do you want to dei oldman ?") 
+#     if(a>120):
+#         print("")   
+        
+# else:
+#     print('you can drive')
+# print("be careful.")       
