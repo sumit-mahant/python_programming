f5fb059df660455032af8b32028425c51d35ba28
sumit mahant <sumitmahant31@gmail.com>
2026-01-04 01:50:53 +0530

    jhkjh
    hbjkhkh

--- match case.py
+++ match case.py
@@ -2,10 +2,10 @@ x=int(input("enter your value"))
 match x:
     case 0:
      print("x is zero.")
-
     case 4:
       print("x is four")
-
-    case _:
-      print(x)
-   
+    case _ if x==99:
+      print("x is  equal to 99")
+      
+    case _ if x==100:
+      print("x is equal to 100")
