f0090528f71c45bde72f8fd1c84dc9bd77a5f6e9
sumit mahant <sumitmahant31@gmail.com>
2026-01-08 20:32:52 +0530

    fjalksfjlk
    alkfjlskjf

--- loops.py
+++ loops.py
@@ -1,24 +1,24 @@
-# name="sumit mahant."
-# for p in name:
-#     print(p)
-#     if(p=="m"):
-#         print("from here sirname has started.")
+name="sumit mahant."
+for p in name:
+    print(p)
+    if(p=="m"):
+        print("from here sirname has started.")
 
 
-# colors=["green","red","yellow","chutiya",]
-# for x in colors:
-#     print(x)
-#     for i in x:
-#         print(i)
+colors=["green","red","yellow","chutiya",]
+for x in colors:
+    print(x)
+    for i in x:
+        print(i)
 
-# for k in range(500):
-#  print(k+1)
+for k in range(500):
+ print(k+1)
 
-# for k in range(1,20001):
-#    print(k)
+for k in range(1,20001):
+   print(k)
 
-# for p in range(1,22,5):
-#  print(p)
+for p in range(1,22,5):
+ print(p)
 
 i=6
 while(i<30):
@@ -30,3 +30,17 @@ while(p<=30):
  p=int(input("enter the value:"))
 print(p)
 print("I am happy now.") 
+i=0
+while(i<=30):
+    print(i)
+    i=i+3
+
+for p in range(10,25,2):
+    print(p)
+
+num=int(input("Enter your value:"))
+while(num<30):
+  num=int(input("Enter your value:"))
+  print(num)
+
+print("code is over.")
