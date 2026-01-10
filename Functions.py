03d60f9327b9bb11529926f3d891cae84b92032a
sumit mahant <sumitmahant31@gmail.com>
2026-01-10 17:03:24 +0530

    gjkhgj
    ghjkljkl

--- Functions.py
+++ Functions.py
@@ -2,20 +2,27 @@ def Gmean(a,b):
     mean=(a*b/(a+b))
     print(mean)
 
-# def 
+def isgreater(a,b):
+    if(a>b):
+     print("first number is greater.")
+
+    else:
+     print("second number is greater.")
 a=10
 b=12
-if(a>b):
-    print("first number is greater.")
+isgreater(a,b)
+# if(a>b):
+#     print("first number is greater.")
 
-else:
-    print("second number is greater.")
+# else:
+#     print("second number is greater.")
 Gmean(a,b)
 c=26
 d=20
-if(c>d):
-    print("first number is greater.")
+isgreater(c,d)
+# if(c>d):
+#     print("first number is greater.")
 
-else:
-    print("second number is greater.")
+# else:
+#     print("second number is greater.")
 Gmean(c,d)
