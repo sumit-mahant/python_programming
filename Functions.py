149d2d287d9d8eefd9a7f6e4e8df9b2023af080d
sumit mahant <sumitmahant31@gmail.com>
2026-01-10 16:56:49 +0530

    uhjkhkj
    kl;hklnh

---
+++ Functions.py
@@ -0,0 +1,21 @@
+def Gmean(a,b):
+    mean=(a*b/(a+b))
+    print(mean)
+
+# def 
+a=10
+b=12
+if(a>b):
+    print("first number is greater.")
+
+else:
+    print("second number is greater.")
+Gmean(a,b)
+c=26
+d=20
+if(c>d):
+    print("first number is greater.")
+
+else:
+    print("second number is greater.")
+Gmean(c,d)
--- loops.py
+++ loops.py
@@ -1,57 +1,57 @@
-# name="sumit mahant."
-# for p in name:
-#     print(p)
-#     if(p=="m"):
-#         print("from here sirname has started.")
+name="sumit mahant."
+for p in name:
+    print(p)
+    if(p=="m"):
+        print("from here sirname has started.")
 
 
-# colors=["green","red","yellow","chutiya",]
-# for x in colors:
-#     print(x)
-#     for i in x:
-#         print(i)
+colors=["green","red","yellow","chutiya",]
+for x in colors:
+    print(x)
+    for i in x:
+        print(i)
 
-# for k in range(500):
-#  print(k+1)
+for k in range(50):
+ print(k+1)
 
-# for k in range(1,20001):
-#    print(k)
+for k in range(1,20):
+   print(k)
 
-# for p in range(1,22,5):
-#  print(p)
+for p in range(1,22,5):
+ print(p)
 
-# i=6
-# while(i<30):
-#  print(i)
-#  i=i+5
+i=6
+while(i<30):
+ print(i)
+ i=i+5
 
-# p=int(input("enter the value:"))
-# while(p<=30):
-#  p=int(input("enter the value:"))
-# print(p)
-# print("I am happy now.") 
-# i=0
-# while(i<=30):
-#     print(i)
-#     i=i+3
+p=int(input("enter the value:"))
+while(p<=30):
+ p=int(input("enter the value:"))
+print(p)
+print("I am happy now.") 
+i=0
+while(i<=30):
+    print(i)
+    i=i+3
 
-# for p in range(10,25,2):
-#     print(p)
+for p in range(10,25,2):
+    print(p)
 
-# num=int(input("Enter your value:"))
-# while(num<30):
-#   num=int(input("Enter your value:"))
-#   print(num)
+num=int(input("Enter your value:"))
+while(num<30):
+  num=int(input("Enter your value:"))
+  print(num)
 
-# print("code is over.")
-# count=8
-# while(count>0):
-#     print(count)
-#     count=count-1
-# else:
-#     print("now I am happy.")
-# 
-#     
+print("code is over.")
+count=8
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("now I am happy.")
+
+    
 # continue and break in python 
 for i in range(100):
     
@@ -60,12 +60,12 @@ for i in range(100):
         continue
     print("5X",i+1,"=",5*(i+1))
 
-# for p in range(1,25,3):
-#     if(p==20):
-#         break
+for p in range(1,25,3):
+    if(p==20):
+        break
        
-#     print("6 X",p,"=",6*p)
-# print("I am out from this.") 
+    print("6 X",p,"=",6*p)
+print("I am out from this.") 
 # Do while loop 
 i=0
 while True:
