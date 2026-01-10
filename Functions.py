b7b744b160d3174d7967471432a223218b868e77
sumit mahant <sumitmahant31@gmail.com>
2026-01-10 17:17:33 +0530

    sdfvdsgd
    adfadfa

--- Functions.py
+++ Functions.py
@@ -8,21 +8,32 @@ def isgreater(a,b):
 
     else:
      print("second number is greater.")
+
+def islesser(a,b):
+   if(a<b):
+      print("first number is smaller.")
+
+   else:
+      print("second number is smaller.")     
 a=10
 b=12
 isgreater(a,b)
+Gmean(a,b)
+islesser(a,b)
 # if(a>b):
 #     print("first number is greater.")
 
 # else:
 #     print("second number is greater.")
-Gmean(a,b)
+print("new code has came.")
 c=26
 d=20
 isgreater(c,d)
+Gmean(c,d)
+islesser(c,d)
 # if(c>d):
 #     print("first number is greater.")
 
 # else:
 #     print("second number is greater.")
-Gmean(c,d)
+
