35dbf266ba741754b290b73b51eb79247d2b4458
sumit mahant <sumitmahant31@gmail.com>
2026-01-10 12:49:59 +0530

    ksadjflk
    jfalkj;d

--- loops.py
+++ loops.py
@@ -44,9 +44,28 @@
 #   print(num)
 
 # print("code is over.")
-count=8
-while(count>0):
-    print(count)
-    count=count-1
-else:
-    print("now I am happy.")    
+# count=8
+# while(count>0):
+#     print(count)
+#     count=count-1
+# else:
+#     print("now I am happy.")
+# 
+#     
+# continue and break in python 
+# for i in range(10):
+    
+    # if(i==6):
+    #     print("skip the iteration")
+    #     continue
+    # print("5X",i+1,"=",5*(i+1))
+
+for p in range(1,25,3):
+    if(p==20):
+        break
+       
+    print("6 X",p,"=",6*p)
+print("I am out from this.")      
+
+
+    
