c733feb7f55c80ba51181bb94b59fee588c1c984
sumit mahant <sumitmahant31@gmail.com>
2026-01-13 01:05:28 +0530

    amma
    pull

--- Functions.py
+++ Functions.py
@@ -20,11 +20,11 @@ b=12
 isgreater(a,b)
 Gmean(a,b)
 islesser(a,b)
-# if(a>b):
-#     print("first number is greater.")
+if(a>b):
+    print("first number is greater.")
 
-# else:
-#     print("second number is greater.")
+else:
+    print("second number is greater.")
 print("new code has came.")
 c=26
 d=20
