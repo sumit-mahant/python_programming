789981ad2512234327012250c085345322e78b1e
sumit mahant <sumitmahant31@gmail.com>
2026-01-16 01:17:15 +0530

    egdgdg
    dgdgdg

--- Functions.py
+++ Functions.py
@@ -31,9 +31,33 @@ d=20
 isgreater(c,d)
 Gmean(c,d)
 islesser(c,d)
-# if(c>d):
-#     print("first number is greater.")
+if(c>d):
+    print("first number is greater.")
 
-# else:
-#     print("second number is greater.")
+else:
+    print("second number is greater.")
+#DEFAULT ARGUMENTS
+def average(a=3,b=10):
+   print("the average is ",((a+b)/2))
+
+average(b=5)
+def name(fname, mname="love",lname="paro"):
+   print("your name is",fname,mname,lname)
+name ("sumit",)
+#KEYWORD ARGUMENTS--IN THIS ORDER DOSE note MATTER!
+def average(a,b):
+   print("the average is ",((a+b)/2))
+
+average(b=5,a=10)
+average(a=10,b=5)
+def average(*numbers):
+    sum=0
+    for i in numbers:
+        sum=sum+i
+    print("average of the numbers=",sum/len(numbers))
+
+average(1,2,3,4,5,6,7,8,11)
+
+
+
 
