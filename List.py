78bb21a4e84d58b240393986a60128382a22188c
sumit mahant <sumitmahant31@gmail.com>
2026-01-25 18:50:14 +0530

    gbfg
    dgdfg

---
+++ List.py
@@ -0,0 +1,34 @@
+# marks=[1,3,4,5,6,]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# name=["sumit",3,"paro",1,True,10.3,4,15,17,22,45]
+# print(name)
+# print(name[0]) #positive index 
+# print(name[-3])#negative indexing
+# print(name[len(name)-3])#meaning of negative indexing
+# # print(name[3])
+# # print(name[4])
+# if "paro" in name:
+#     print("yes")
+# else:
+#     print("no") 
+# if "umip" in "sumit":
+#     print("yep")
+# else:
+#     print("no!")
+# print(name)
+# print(name[0:-1:2])
+# lst= [i for i in range(100) if i%2==0]
+# print(len(lst))
+#  #METHODES IN LIST 
+l=[1,2,3,4,5,6,7,8]
+print(l[7:9])
+l.append(2)
+print(l)
+
+
+
+
